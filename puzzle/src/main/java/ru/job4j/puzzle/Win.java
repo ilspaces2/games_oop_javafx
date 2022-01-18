c9b422384a9571d156955b8e2a07e164770ffe80
I_M <moskva.88@mail.ru>
2022-01-18 20:49:22 +0300

    2. Сокобан. Тестовое задание. [#75283 #252110]

--- puzzle/src/main/java/ru/job4j/puzzle/Win.java
+++ puzzle/src/main/java/ru/job4j/puzzle/Win.java
@@ -2,36 +2,34 @@ package ru.job4j.puzzle;
 
 public class Win {
     public static boolean check(int[][] board) {
-        return checkHorizontal(board) || checkVertical(board);
-    }
-
-    public static boolean checkHorizontal(int[][] board) {
         boolean rzl = false;
-        for (int i = 0; i < board.length; i++) {
-            if (board[i][i] == 1) {
+        for (int index = 0; index < board.length; index++) {
+            if (board[index][index] == 1 && checkHorizontal(board, index)
+                    || checkVertical(board, index)) {
                 rzl = true;
-                for (int row = 0; row < board.length; row++) {
-                    if (board[row][i] == 0) {
-                        rzl = false;
-                        break;
-                    }
-                }
+                break;
             }
         }
         return rzl;
     }
 
-    public static boolean checkVertical(int[][] board) {
-        boolean rzl = false;
-        for (int i = 0; i < board.length; i++) {
-            if (board[i][i] == 1) {
-                rzl = true;
-                for (int cell = 0; cell < board.length; cell++) {
-                    if (board[i][cell] == 0) {
-                        rzl = false;
-                        break;
-                    }
-                }
+    public static boolean checkHorizontal(int[][] board, int row) {
+        boolean rzl = true;
+        for (int index = 0; index < board.length; index++) {
+            if (board[row][index] == 0) {
+                rzl = false;
+                break;
+            }
+        }
+        return rzl;
+    }
+
+    public static boolean checkVertical(int[][] board, int column) {
+        boolean rzl = true;
+        for (int index = 0; index < board.length; index++) {
+            if (board[index][column] == 0) {
+                rzl = false;
+                break;
             }
         }
         return rzl;
